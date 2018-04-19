e7f91bea5790293ef590a5a9e8e1171aca36ae9b
Pierre Fenoll <pierrefenoll@gmail.com>
2018-04-19 19:01:55 +0200

    master: this will be annoying I can feel it

--- test/otp_vsn_tests.erl
+++ test/otp_vsn_tests.erl
@@ -9,7 +9,7 @@
 do_test_() ->
     [?_assertMatch([_|_], ?OTP_VSN)
 
-    ,?_assert(is_integer(?OTP_VSN_MAJOR) andalso ?OTP_VSN_MAJOR > 0)
+    ,?_assert(is_integer(?OTP_VSN_MAJOR) andalso ?OTP_VSN_MAJOR >  0)
     ,?_assert(is_integer(?OTP_VSN_MINOR) andalso ?OTP_VSN_MINOR >= 0)
     ,?_assert(is_integer(?OTP_VSN_PATCH) andalso ?OTP_VSN_PATCH >= 0)
 
@@ -17,22 +17,22 @@ do_test_() ->
     ,?_assertMatch([_|_], ?OTP_VSN_MINOR_STRING)
     ,?_assertMatch([_|_], ?OTP_VSN_PATCH_STRING)
 
-     %% More tests if running inside CI
-    ] ++
+    ] ++ %% More tests if running inside CI
         lists:flatten(
-          [[?_assertNotEqual(false, OTP_VSN)
+          [[?_assertMatch([_|_], OTP_VSN)
            ,?_assertEqual(OTP_VSN, ?OTP_VSN)
+           ,?_assertNotEqual(nomatch, re:run(<<?OTP_VSN>>, <<"^[0-9]+\\.[0-9]+\\.[0-9]+$">>))
 
-           ,?_assertNotEqual(false, OTP_VSN_M)
-           ,?_assertNotEqual(false, OTP_VSN_m)
-           ,?_assertNotEqual(false, OTP_VSN_P)
-           ,?_assertEqual(OTP_VSN_M, ?OTP_VSN_MAJOR)
-           ,?_assertEqual(OTP_VSN_m, ?OTP_VSN_MINOR)
-           ,?_assertEqual(OTP_VSN_P, ?OTP_VSN_PATCH)
+           ,?_assertMatch([_|_], OTP_VSN_M)
+           ,?_assertMatch([_|_], OTP_VSN_m)
+           ,?_assertMatch([_|_], OTP_VSN_P)
+           ,?_assertEqual(list_to_integer(OTP_VSN_M), ?OTP_VSN_MAJOR)
+           ,?_assertEqual(list_to_integer(OTP_VSN_m), ?OTP_VSN_MINOR)
+           ,?_assertEqual(list_to_integer(OTP_VSN_P), ?OTP_VSN_PATCH)
 
-           ,?_assertEqual(integer_to_list(OTP_VSN_M), ?OTP_VSN_MAJOR_STRING)
-           ,?_assertEqual(integer_to_list(OTP_VSN_m), ?OTP_VSN_MINOR_STRING)
-           ,?_assertEqual(integer_to_list(OTP_VSN_P), ?OTP_VSN_PATCH_STRING)
+           ,?_assertEqual(OTP_VSN_M, ?OTP_VSN_MAJOR_STRING)
+           ,?_assertEqual(OTP_VSN_m, ?OTP_VSN_MINOR_STRING)
+           ,?_assertEqual(OTP_VSN_P, ?OTP_VSN_PATCH_STRING)
            ]
            || os:getenv("CI") =:= "true",
               {OTP_VSN
